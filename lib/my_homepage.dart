480b9245742356b1126f3b6940540c432b04cf5b
lionking <bahbeshiel@gmail.com>
2022-02-12 22:36:50 +0000

    first screen half way

--- lib/my_homepage.dart
+++ lib/my_homepage.dart
@@ -3,15 +3,6 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -19,20 +10,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
-
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-      ),
-      body: Container(
-        
-      ),
-      
-  );
+      body: Stack(children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset(
+            "images/bgbg.jpg",
+            fit: BoxFit.cover,
+          ),
+        ),
+       
+        Padding(
+          padding: const EdgeInsets.only(top: 90.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 400,
+            child: Image.asset(
+              "images/guy.png",
+              fit: BoxFit.cover,
+            )),
+        ),
+
+        Padding(
+          padding: const EdgeInsets.only(top: 450.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            
+            decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                  ),
+          ),
+        )
+      ]),
+    );
   }
 }
