931840c9862ce68180a5b2748742aa2300f506a6
King Bethel <bethelmelariri@gmail.com>
2022-02-13 19:08:26 +0000

    Created my page layout

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -31,7 +31,72 @@ class _SecondPageState extends State<SecondPage> {
                 fit: BoxFit.cover,
               )),
         ),
-        Padding(),
+        Padding(
+          
+          padding: const EdgeInsets.only(top: 450.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 45.0),
+                    child: Text(
+                      "",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                    ),
+                  ),
+                  const Padding(
+                     padding: EdgeInsets.all(20.0),
+                      child: Center(
+                        child: Text(
+                          "",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 20,
+                              color: Colors.grey),
+                        ),
+                      )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 100.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(left: 20),
+                          child: TextButton(
+                              onPressed: () {},
+                              child: const Text("Skip",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.black))),
+                        ),
+                        Expanded(child: Container()),
+                        Container(
+                          margin: const EdgeInsets.only(right: 20),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(500)),
+                              primary: Colors.black, // background
+                              onPrimary: Colors.white,
+                              // foreground
+                            ),
+                            onPressed: () {},
+                            child: const Text('Next'),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ]),
+          ),
+        
+        ),
       ]),
     
     );
