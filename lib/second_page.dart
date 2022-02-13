46f38ce5afa23cc6d5c769d2725e27ad5b771aa7
King Bethel <bethelmelariri@gmail.com>
2022-02-13 19:07:17 +0000

    Imported images to my codebase

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -10,6 +10,30 @@ class SecondPage extends StatefulWidget {
 class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      
+      body: Stack(children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Image.asset(
+            "images/vector.jpg",
+            fit: BoxFit.cover,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 90.0),
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 400,
+              child: Image.asset(
+                "images/Brock.png",
+                fit: BoxFit.cover,
+              )),
+        ),
+        Padding(),
+      ]),
+    
+    );
   }
 }
