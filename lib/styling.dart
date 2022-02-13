94bd2b789d266316cffbc3867a10f5908144736c
afranewuoasare <timothy@farmhero.africa>
2022-02-13 22:02:36 +0000

    added description two

--- lib/styling.dart
+++ lib/styling.dart
@@ -10,3 +10,6 @@ Text thirdHeadingText = Text("`let's go...");
 
 Text descriptionoOne = Text(
     "Explore the all new way to build the fitness & find the perfect fit for you");
+
+Text descriptionnTwo = Text(
+    "Feel free to adust your workout & diet settingns accordinng to your comfort");
